f678d3f86df48b0dd6044d782295fca1a198f86a
tolik <121683889+digittolik@users.noreply.github.com>
2023-01-09 03:06:01 +0300

    id
    всё  таки я не смог избавиться от почти полностью реализованной заглушки в виде id, уж очень много механик я пытался к ней привязать, восстановил и немного доработал. Я не придумал как выдавать абсолютно уникальные id юзерам, в пределах одной группы ещё можно было придумать, но независимые группы... Разве что делать в базе данных список и выдавать новые id. Но базу данных я так и не реализовал, хотя она была нужна.

--- Group.rs
+++ Group.rs
@@ -1,4 +1,5 @@
 
+
 #[derive(Debug)]
 enum Access {
     Public,
@@ -7,6 +8,7 @@ enum Access {
 
 
 pub struct Group {
+    id: u64,
     access: Access,
     members: Vec<(u64, bool)>,
 }
@@ -15,11 +17,15 @@ pub struct Group {
 impl Group {
     pub async fn new(admin: u64) -> Group {
         Group {
+            id::get_id(Types::Group).await,
             access: Access::Public,
             members: vec![(admin, true)],
         }
     }
-   
-    pub async fn print_info(&self) {
-        println!("\nGROUP INFO\n{}\n{:?}\n{:?}\n",self.access, self.members);
+    pub async fn get_id(&self) -> u64 {
+        self.id
     }
+    pub async fn print_info(&self) {
+        println!("\nGROUP INFO\n{}\n{:?}\n{:?}\n", self.id, self.access, self.members);
+    }
+}
