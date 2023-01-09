e828104d492cab91d1f483acb6c596136fb3e09e
tolik <121683889+digittolik@users.noreply.github.com>
2023-01-09 03:14:04 +0300

    id
    восстановление заглушки в виде id. id должно было бытьв базе данных...

--- Group.rs
+++ Group.rs
@@ -1,31 +1,61 @@
-
+#[path = "group.rs"] mod group;
+pub use group::Group;
 
 #[derive(Debug)]
-enum Access {
-    Public,
-    Private,
-}
-
-
-pub struct Group {
+pub struct User {
     id: u64,
-    access: Access,
-    members: Vec<(u64, bool)>,
+    nickname: String,
+    password: String,
+    groups: Vec<u64>,
 }
 
 
-impl Group {
-    pub async fn new(admin: u64) -> Group {
-        Group {
-            id::get_id(Types::Group).await,
-            access: Access::Public,
-            members: vec![(admin, true)],
+impl User {
+    pub async fn new() -> User {
+        User {
+            id::get_id(Types::User).await,
+            nickname: String::new(),
+            password: String::new(),
+            groups: Vec::new(),
         }
+    }
+    pub async fn new_with_fields(nickname: String, password: String) -> User {
+        User {
+            id:::get_id(Types::User).await,
+            nickname,
+            password,
+            groups: Vec::new(),
+        }
+    }
+    pub async fn set_id(&mut self, id: u64) {
+        self.id = id
+    }
+    pub async fn set_nickname(&mut self, name: String) {
+        self.nickname = name
+    }
+    pub async fn set_password(&mut self, password: String) {
+        self.password = password
     }
     pub async fn get_id(&self) -> u64 {
         self.id
     }
+    pub async fn get_nickname(&self) -> &String {
+        &self.nickname
+    }
+    pub async fn get_password(&self) -> &String {
+        &self.password
+    }
+    pub async fn get_groups(&self) -> &Vec<u64> {
+        &self.groups
+    }
     pub async fn print_info(&self) {
-        println!("\nGROUP INFO\n{}\n{:?}\n{:?}\n", self.id, self.access, self.members);
+        println!("\nUSER INFO:\n{}\n{}\n{}\n{:?}\n", self.id, self.nickname, self.password, self.groups)
+    }
+    pub async fn create_group(&mut self) -> Group {
+        let new_group: Group = Group::new(self.id).await;
+        self.groups.push(new_group.get_id().await);
+        
+        new_group.print_info().await;
+        new_group
     }
 }
